00c0dc0f428244932be93b2024dca75bc69ace60
sebrahim <mohd.s.aqeel@gmail.com>
2024-09-20 15:35:17 +0300

    [ADD] odoo_module: Create data-only module for custom products and partners
    - Modified the default Odoo module scaffold:
      - Removed unnecessary structure, leaving only essential components to meet the assignment requirements.
    - `custom_products_partners` module will  manage custom consumable products and partner data:
      - Added `__manifest__.py` and `__init__.py` for module configuration.
      - `data/products.xml` and `data/partners.xml` currently empty for future data input.
    - Updated `.gitignore` to exclude additional related files such as virtual environments and logs.

---
+++ odoo_module/custom_products_partners/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Custom Products and Partners',
+
+    'summary': "Data-only module with custom consumable product and partner",
+
+    'description': """
+This module provides a custom consumable product and a custom partner for Take Off assignment. It is designed to demonstrate the creation and management of products and partners using Odoo's data files.
+    """,
+
+    'author': "Sayed Mohamed Ebrahim",
+    'website': "https://www.sayedmohd.com",
+
+    'category': 'Sales',
+    'version': '0.1',
+
+    'depends': ['base', 'product'],
+
+    'data': [
+        'data/products.xml',
+        'data/partners.xml',
+    ],
+
+    'license': 'LGPL-3',
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
